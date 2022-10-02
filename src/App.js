77690e20a90a8da6db77ce04905ba6df28667a50
TheDoru <thedoruv2@gmail.com>
2022-10-02 15:07:10 +0200

    Edited app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div></div>;
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import "./styles/app.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
